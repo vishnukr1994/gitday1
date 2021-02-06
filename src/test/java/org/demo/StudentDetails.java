ac93531fc2d406a049da4312479e15316cd3eb50
vishnukr1994 <vishnudathkr@gmail.com>
2021-02-06 17:45:03 +0530

    DONE

---
+++ src/test/java/org/demo/StudentDetails.java
@@ -0,0 +1,14 @@
+package org.demo;
+
+public class StudentDetails {
+	private void StudentId() {
+		int StudentId = 124;
+		System.out.println(StudentId);
+		}
+		private void studentName() {
+			String studentName = "vishnu";
+			System.out.println(studentName);
+
+		}
+
+}
