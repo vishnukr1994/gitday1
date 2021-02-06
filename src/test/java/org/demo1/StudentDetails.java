b170d9dea60bc9568eef62ed28b753b1ccee067b
vishnukr1994 <vishnudathkr@gmail.com>
2021-02-06 19:21:54 +0530

    newly added

---
+++ src/test/java/org/demo1/StudentDetails.java
@@ -0,0 +1,15 @@
+package org.demo1;
+
+public class StudentDetails {
+	
+		private void StudentId() {
+			int StudentId = 124;
+			System.out.println(StudentId);
+			}
+			private void studentName() {
+				String studentName = "vishnu";
+				System.out.println(studentName);
+
+			}
+
+}
